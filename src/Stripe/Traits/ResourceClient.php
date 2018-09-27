77bf0b997506519ccd08ff7e25b9d3667a08dad4
Connor Forsyth <connor@randomstate.co.uk>
2018-09-27 10:16:47 +0100

    Better reflect function of classes via name.

--- src/Stripe/Traits/ResourceClient.php
+++ src/Stripe/Traits/ResourceClient.php
@@ -1,10 +1,10 @@
 <?php
 
 
-namespace RandomState\Stripe\Stripe\Traits;
+namespace RandomState\Stripe;
 
 
-trait Resource
+trait ResourceClient
 {
     abstract public function getResourceClass();
     abstract protected function options();

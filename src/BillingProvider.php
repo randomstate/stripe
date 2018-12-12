09519838bb207b432fe53525130ffcc2c4b54349
Connor Forsyth <connorimrie@outlook.com>
2018-12-12 17:26:37 +0000

    Add usage records to main billing providers.

--- src/BillingProvider.php
+++ src/BillingProvider.php
@@ -2,23 +2,66 @@
 
 namespace RandomState\Stripe;
 
+use RandomState\Stripe\Contracts\Charges;
+use RandomState\Stripe\Contracts\Coupons;
+use RandomState\Stripe\Contracts\Customers;
+use RandomState\Stripe\Contracts\Plans;
+use RandomState\Stripe\Contracts\Products;
+use RandomState\Stripe\Contracts\Refunds;
+use RandomState\Stripe\Contracts\Sources;
+use RandomState\Stripe\Contracts\SubscriptionItems;
+use RandomState\Stripe\Contracts\Tokens;
+use RandomState\Stripe\Contracts\UsageRecords;
+
 interface BillingProvider
 {
+    /**
+     * @return Charges
+     */
     public function charges();
 
+    /**
+     * @return Customers
+     */
     public function customers();
 
+    /**
+     * @return Products
+     */
     public function products();
 
+    /**
+     * @return Refunds
+     */
     public function refunds();
 
+    /**
+     * @return Tokens
+     */
     public function tokens();
 
+    /**
+     * @return Sources
+     */
     public function sources();
 
+    /**
+     * @return Coupons
+     */
     public function coupons();
 
+    /**
+     * @return Plans
+     */
     public function plans();
 
+    /**
+     * @return SubscriptionItems
+     */
     public function subscriptions();
+
+    /**
+     * @return UsageRecords
+     */
+    public function usageRecords();
 }
